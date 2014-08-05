cabc630c6ba5fd15cd63af99a8151d76efbc1cbd
NanneHuiges <nanne@huiges.nl>
2014-08-05 22:00:26 +0200

    Version bump
    Ready for new development, version bump and verbosity

--- src/com/huiges/AndroBlip/C.java
+++ src/com/huiges/AndroBlip/C.java
@@ -23,7 +23,7 @@ public class C {
 	 * Loglevel value. Based on this value the
 	 * constants VERBOSE, DEBUG or MIN are true/false
 	 */
-	public static int LOGLVL 		 = 0;
+	public static int LOGLVL 		 = 11;
 	
 	/**
 	 * Loglevel constants: true/false based on LOGLVL value.
